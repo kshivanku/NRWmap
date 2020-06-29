dc8087a353c1325080b4d6234a8065f718508c3f
Shivanku Kumar <kshivanku@gmail.com>
2020-06-29 09:50:14 -0700

    added back the icons

---
+++ src/components/Source.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Source.css'
+
+const Source = () => (
+    <div className="sourceContainer">
+        <a target="_blank" href="https://iwaponline.com/ws/article/19/3/831/41417/Quantifying-the-global-non-revenue-water-problem">
+            Source: IWA
+        </a>
+    </div>
+)
+
+export default Source
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import {useState, useEffect} from 'react';
 import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import Tabletop from 'tabletop';
+import Source from '../components/Source'
 import './index.css'
 
 const IndexPage = () => {
@@ -11,7 +12,7 @@ const IndexPage = () => {
     height: "100vh",
     latitude: 23.8859,
     longitude: 45.0792,
-    zoom: 1
+    zoom: 2
   })
 
   const [selectedCountry, setSelectedCountry] = useState(null)
@@ -19,11 +20,15 @@ const IndexPage = () => {
   const setBackgroundColor = (nrw) => {
     if(nrw < 20) {
       return "#47CF73"
-    } else if (nrw > 40) {
+    } else if (nrw >= 40) {
       return "#FF3C41"
     } else {
       return "#FCD000"
     }
+  }
+
+  const setMarkerSize = (pop) => {
+    
   }
 
   const [data, setData] = useState([]);
@@ -49,62 +54,70 @@ const IndexPage = () => {
   }, [])
 
   return (
-    <ReactMapGL 
-      {...viewport} 
-      mapboxApiAccessToken={process.env.GATSBY_MAPBOX_TOKEN}
-      mapStyle="mapbox://styles/kshivanku/ck4vr9icb2z261clkwtwf076v"
-      onViewportChange = {nextViewport => setViewport(nextViewport)}
-    >
-      {data.map(item => (
-        <Marker
-          key={item["Supplied population"]}
-          latitude = {Number(item.latitude)}
-          longitude = {Number(item.longitude)}
-        >
-          <button
-            className = "marker" 
-            style={{backgroundColor:`${setBackgroundColor(Number(item['Level of NRW %']))}`}}
-            onClick = {() => {
-              setSelectedCountry(item)
-            }}
+    <>
+      <ReactMapGL 
+        {...viewport} 
+        mapboxApiAccessToken={process.env.GATSBY_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/kshivanku/ck4vr9icb2z261clkwtwf076v"
+        onViewportChange = {nextViewport => setViewport(nextViewport)}
+      >
+        {data.map(item => (
+          <Marker
+            key={item["Supplied population"]}
+            latitude = {Number(item.latitude)}
+            longitude = {Number(item.longitude)}
           >
-          </button>
-        </Marker>
-      ))}
-      {selectedCountry && (
-        <Popup
-          latitude = {Number(selectedCountry.latitude)}
-          longitude = {Number(selectedCountry.longitude)}
-          onClose = {() => setSelectedCountry(null)}
-        >
-          <div className="popup">
-            <h1>{selectedCountry.Country}</h1>
-            <ul>
-              <li>
-                <p className="key">Level of NRW %: </p>
-                <p className="value">{selectedCountry["Level of NRW %"]}</p>
-              </li>
-              <li>
-                <p className="key">% of urban population using piped water: </p>
-                <p className="value">{selectedCountry["% of urban population using piped water"]}</p>
-              </li>
-              <li>
-                <p className="key">Supplied population: </p>
-                <p className="value">{selectedCountry["Supplied population"]}</p>
-              </li>
-              <li>
-                <p className="key">Free from contamination: </p>
-                <p className="value">{selectedCountry["Free from contamination"]}</p>
-              </li>
-              <li>
-                <p className="key">Available when needed: </p>
-                <p className="value">{selectedCountry["Available when needed"]}</p>
-              </li>
-            </ul>
-          </div>
-        </Popup>
-      )}
-    </ReactMapGL>
+            <button
+              className = "marker" 
+              style={{
+                // width: `${Number(item["Supplied population"])/10000000}px`,
+                // height: `${Number(item["Supplied population"])/10000000}px`,
+                // borderRadius: `${Number(item["Supplied population"])/10000000}px`,
+                backgroundColor:`${setBackgroundColor(Number(item['Level of NRW %']))}`
+              }}
+              onClick = {() => {
+                setSelectedCountry(item)
+              }}
+            >
+            </button>
+          </Marker>
+        ))}
+        {selectedCountry && (
+          <Popup
+            latitude = {Number(selectedCountry.latitude)}
+            longitude = {Number(selectedCountry.longitude)}
+            onClose = {() => setSelectedCountry(null)}
+          >
+            <div className="popup">
+              <h1>{selectedCountry.Country}</h1>
+              <ul>
+                <li>
+                  <p className="key">Level of NRW %: </p>
+                  <p className="value">{selectedCountry["Level of NRW %"]}</p>
+                </li>
+                <li>
+                  <p className="key">% of urban population using piped water: </p>
+                  <p className="value">{selectedCountry["% of urban population using piped water"]}</p>
+                </li>
+                <li>
+                  <p className="key">Supplied population: </p>
+                  <p className="value">{selectedCountry["Supplied population"]}</p>
+                </li>
+                <li>
+                  <p className="key">Free from contamination: </p>
+                  <p className="value">{selectedCountry["Free from contamination"]}</p>
+                </li>
+                <li>
+                  <p className="key">Available when needed: </p>
+                  <p className="value">{selectedCountry["Available when needed"]}</p>
+                </li>
+              </ul>
+            </div>
+          </Popup>
+        )}
+      </ReactMapGL>
+      <Source />
+    </>
   );
 }
 
