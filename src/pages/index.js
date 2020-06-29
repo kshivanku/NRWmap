c6268527748767932ab89f97bf045256ea358103
Shivanku Kumar <kshivanku@gmail.com>
2020-06-29 02:40:27 -0700

    map v1 complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,111 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import {useState, useEffect} from 'react';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import Tabletop from 'tabletop';
+import './index.css'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = () => {
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 23.8859,
+    longitude: 45.0792,
+    zoom: 1
+  })
+
+  const [selectedCountry, setSelectedCountry] = useState(null)
+
+  const setBackgroundColor = (nrw) => {
+    if(nrw < 20) {
+      return "#47CF73"
+    } else if (nrw > 40) {
+      return "#FF3C41"
+    } else {
+      return "#FCD000"
+    }
+  }
+
+  const [data, setData] = useState([]);
+
+  useEffect(()=> {
+    Tabletop.init({
+      key: '1YvbT0-2VkS3cNYpwkgiQBzB0UMfsS-DTmbo-vsmoy48',
+      callback: googleData => {
+        console.log('google sheet data --->', googleData);
+        setData(googleData)
+      },
+      simpleSheet: true
+    })
+
+    const listener = (e) => {
+      if(e.key === "Escape") {
+        setSelectedCountry(null)
+      }
+    }
+
+    window.addEventListener("keydown", listener)
+
+  }, [])
+
+  return (
+    <ReactMapGL 
+      {...viewport} 
+      mapboxApiAccessToken={process.env.GATSBY_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/kshivanku/ck4vr9icb2z261clkwtwf076v"
+      onViewportChange = {nextViewport => setViewport(nextViewport)}
+    >
+      {data.map(item => (
+        <Marker
+          key={item["Supplied population"]}
+          latitude = {Number(item.latitude)}
+          longitude = {Number(item.longitude)}
+        >
+          <button
+            className = "marker" 
+            style={{backgroundColor:`${setBackgroundColor(Number(item['Level of NRW %']))}`}}
+            onClick = {() => {
+              setSelectedCountry(item)
+            }}
+          >
+          </button>
+        </Marker>
+      ))}
+      {selectedCountry && (
+        <Popup
+          latitude = {Number(selectedCountry.latitude)}
+          longitude = {Number(selectedCountry.longitude)}
+          onClose = {() => setSelectedCountry(null)}
+        >
+          <div className="popup">
+            <h1>{selectedCountry.Country}</h1>
+            <ul>
+              <li>
+                <p className="key">Level of NRW %: </p>
+                <p className="value">{selectedCountry["Level of NRW %"]}</p>
+              </li>
+              <li>
+                <p className="key">% of urban population using piped water: </p>
+                <p className="value">{selectedCountry["% of urban population using piped water"]}</p>
+              </li>
+              <li>
+                <p className="key">Supplied population: </p>
+                <p className="value">{selectedCountry["Supplied population"]}</p>
+              </li>
+              <li>
+                <p className="key">Free from contamination: </p>
+                <p className="value">{selectedCountry["Free from contamination"]}</p>
+              </li>
+              <li>
+                <p className="key">Available when needed: </p>
+                <p className="value">{selectedCountry["Available when needed"]}</p>
+              </li>
+            </ul>
+          </div>
+        </Popup>
+      )}
+    </ReactMapGL>
+  );
+}
 
 export default IndexPage
